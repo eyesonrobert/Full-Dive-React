a42bec6602ec7bf7e49910d47f21939f5b348d0e
Robert Franklin <eyesonrobert@gmail.com>
2018-10-08 10:16:48 -0700

    minor styling applied

--- src/App.js
+++ src/App.js
@@ -23,24 +23,49 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 29},
+        {name: "Stephanie", age: 27}
+      ]
+    })
+  }
+
   render() {
+
+    const style={
+      backgroundColor:'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding:'8px',
+      cursor:'pointer'
+    };
     return (
       <div className="App">
         <h1>This is a Title</h1>
         <p>This is really working</p>
-        <button onClick={() =>this.switchNameHandler('Mazamillian!!')}>Switch Name</button> 
+        <button 
+        style={style}
+        onClick={() =>this.switchNameHandler('Mazamillian!!')}>Switch Name</button> 
                             {/* first method to adjust value */}
+
+        <div>
         <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}/>
         <Person 
         name={this.state.persons[1].name} 
         age={this.state.persons[1].age}
+        changed = {this.nameChangedHandler}
         click={this.switchNameHandler.bind(this, 'Max!')}>Hobbies: racing</Person>
               {/* second (and preferred) method to adjust value */}
+      
         <Person 
         name={this.state.persons[2].name} 
         age={this.state.persons[2].age}/>
+        </div>
       </div>
     );
   }
