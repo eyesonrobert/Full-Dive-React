99df9e04ddbc8a18ad26ac9c30a46ad3a25dcc37
Robert Franklin <eyesonrobert@gmail.com>
2018-10-08 08:59:26 -0700

    Initial Push
    In depth React project utilizing Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 29},
+      {name: "Stephanie", age: 26}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () =>{
+    // console.log('was clicked');
+    // this.state.persons[0].name="Maximillian";
+
+    this.setState({
+      persons: [
+        {name: "Maximillian", age: 28},
+        {name: "Manu", age: 29},
+        {name: "Stephanie", age: 27}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>This is a Title</h1>
+        <p>This is really working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
